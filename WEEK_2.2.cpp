2cdf81f3b0b0b4db91e9f39992f39e94a3fe77a1
mohammadyasmeen0313-ctrl <mohammadyasmeen0313@gmail.com>
2025-10-21 10:41:38 +0530

    week 2.2

---
+++ WEEK_2.2.cpp
@@ -0,0 +1,15 @@
+#include<iostream> 
+#include <stdlib>  
+using namespace std; 
+inline int square(int x) 
+{      
+return x * x;    
+}  
+int main()     
+{      
+system(“color F0”);       
+cout << "Roll No: 24B11AI261" << endl;       
+int num = 3;      
+cout<< “”square of”” <<num <<”is” << square(num) << endl; 
+return 0;    
+}   
